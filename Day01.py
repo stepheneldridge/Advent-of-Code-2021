44311f229981b4ce93723f14b71230cae0a6981c
stepheneldridge <sreldr03@louisville.edu>
2021-12-01 00:16:50 -0500

    Refactored and abstracted moving sum method

--- Day01.py
+++ Day01.py
@@ -1,30 +1,21 @@
-INPUT = open("Day01.txt", 'r')
 nums = []
-for i in INPUT:
-    try:
-        x = int(i)
-        nums.append(x)
-    except:
-        pass
+with open("Day01.txt", 'r') as INPUT:
+    for i in INPUT:
+        try:
+            x = int(i)
+            nums.append(x)
+        except:
+            pass
 
-def get():
-    a = -1
+def get_sum(arr, window):
     last = -1
-    for i in nums:
-        if i > last:
-            a += 1
-        last = i
-    return a
-print("part 1:", get())
-
-def getter():
-    last = -1
-    a = -1
-    for i in range(0, len(nums) - 2):
-        s = nums[i] + nums[i + 1] + nums[i + 2]
+    count = -1
+    for i in range(0, len(arr) - window + 1):
+        s = sum(arr[i:i + window])
         if s > last:
-            a += 1
+            count += 1
         last = s
-    return a
+    return count
 
-print("part 2:", getter())
+print("part 1:", get_sum(nums, 1))
+print("part 2:", get_sum(nums, 3))
