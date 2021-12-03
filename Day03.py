4fb46e2dbc6664b5f0506c8431b4bb3dabc3a755
stepheneldridge <sreldr03@louisville.edu>
2021-12-03 01:13:14 -0500

    Refactored with bitwise fun

--- Day03.py
+++ Day03.py
@@ -1,58 +1,35 @@
 nums = []
+bits = 0
 with open("Day03.txt", 'r') as INPUT:
     for i in INPUT:
-        nums.append(i)
+        bits = max(bits, len(i.strip()))
+        nums.append(int(i, 2))
+
+def get_com(l, i):
+    zeroes = 0
+    for j in l:
+        zeroes += not j & 1 << i
+    return zeroes <= len(l) >> 1
 
 def get_prod_1():
     gamma = 0
-    epsilon = 0
-    for i in range(len(nums[0]) - 1):
-        ones = 0
-        zeroes = 0
-        for j in nums:
-            if j[i] == "1":
-                ones += 1
-            else:
-                zeroes += 1
-        gamma *= 2
-        epsilon *= 2
-        if ones > zeroes:
-            gamma += 1
-        else:
-            epsilon += 1
-    return gamma * epsilon
+    for i in range(bits - 1, -1, -1):
+        gamma  = (gamma << 1) + get_com(nums, i)
+    return gamma * (gamma ^ ((1 << bits) - 1))
 
 print("part 1:", get_prod_1())
 
-def get_com(s, p):
-    ones = 0
-    zeroes = 0
-    for i in s:
-        if i[p] == "1":
-            ones += 1
-        else:
-            zeroes += 1
-    return ones >= zeroes
-
 def get_prod_2():
-    o2match = nums.copy()
-    co2match = nums.copy()
-    for i in range(len(nums[0]) - 1):
-        
-        no2 = []
-        m = str(int(get_com(o2match, i)))
-        for j in o2match:
-            if j[i] == m:
-                no2.append(j)
-        o2match = no2
+    o2 = nums.copy()
+    for i in range(bits - 1, -1, -1):
+        m = get_com(o2, i)
+        o2 = list(filter(lambda v: (v >> i) & 1 == m, o2))
+    co2 = nums.copy()
+    for i in range(bits - 1, -1, -1):
+        m = not get_com(co2, i)
+        co2 = list(filter(lambda v: (v >> i) & 1 == m, co2))
+        if len(co2) <= 1:
+            break
+    return o2[0] * co2[0]
 
-        nco2 = []
-        m = str(int(not get_com(co2match, i)))
-        if len(co2match) <= 1:
-            continue
-        for j in co2match:
-            if j[i] == m:
-                nco2.append(j)
-        co2match = nco2
-    return int(o2match[0], 2) * int(co2match[0], 2)
 print("part 2:", get_prod_2())
